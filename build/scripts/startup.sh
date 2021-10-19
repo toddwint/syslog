5f47637ad5e7597f14b19ef0c73a59d46067e8c2
toddwint <todd.wintermute@gmail.com>
2021-10-19 14:08:45 -0500

    Fixed logrotate not working, added docker attach script

--- build/scripts/startup.sh
+++ build/scripts/startup.sh
@@ -10,11 +10,16 @@ echo "##### Make a remote syslog server #####" >> /etc/syslog-ng/syslog-ng.conf
 echo "source net { udp(); };" >> /etc/syslog-ng/syslog-ng.conf
 echo "destination remote { file("/var/log/remote/syslog.log"); };" >> /etc/syslog-ng/syslog-ng.conf
 echo "log { source(net); destination(remote); };" >> /etc/syslog-ng/syslog-ng.conf
-sed -ri "\|^/var/log/syslog|a /var/log/remote/\*"  /etc/logrotate.d/syslog-ng
+sed -ri "\|^/var/log/syslog|a /var/log/remote/syslog.log"  /etc/logrotate.d/syslog-ng
+sed -ri 's/\#(SYSLOGNG_OPTS="--no-caps")/\1/' /etc/default/syslog-ng
+sed -ri 's|invoke-rc.d syslog-ng reload > /dev/null|service syslog-ng reload|' /etc/logrotate.d/syslog-ng
 
 service syslog-ng stop
+service cron stop
 service syslog-ng start
+service cron start
 service syslog-ng status
+service cron status
 
 frontail -d -p $HTTPPORT /var/log/remote/syslog.log
 
---
+++ run/attach.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Connecting... Use **CTRL-P, CTRL-Q** to exit without stopping the container"
+docker attach syslog
